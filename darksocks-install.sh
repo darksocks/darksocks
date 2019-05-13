6b0c1b5188a08dd727ba755c20f45eb9367cac58
darksocks <vdarksocks@gmail.com>
2019-05-13 22:35:15 +0800

    update install script.

--- darksocks-install.sh
+++ darksocks-install.sh
@@ -1,51 +1,27 @@
 #!/bin/bash
 
 installServer(){
-  if [ ! -d /home/bsrouter ];then
-    useradd bsrouter
-    mkdir -p /home/bsrouter
-    chown -R bsrouter:bsrouter /home/bsrouter
+  if [ ! -d /home/darksocks ];then
+    useradd darksocks
+    mkdir -p /home/darksocks
+    chown -R darksocks:darksocks /home/darksocks
   fi
-  cp -f bsrouter /usr/local/bin/bsrouter
-  cp -f bsconsole /usr/local/bin/bsconsole
-  cp -f bs-ssh.sh /usr/local/bin/bs-ssh
-  cp -f bs-scp.sh /usr/local/bin/bs-scp
-  cp -f bs-sftp.sh /usr/local/bin/bs-sftp
-  ln -sf /usr/local/bin/bsconsole /usr/local/bin/bs-ping
-  ln -sf /usr/local/bin/bsconsole /usr/local/bin/bs-state
-  ln -sf /usr/local/bin/bsconsole /usr/local/bin/bs-bash
-  if [ ! -f /etc/systemd/system/bsrouter.service ];then
-    cp -f bsrouter.service /etc/systemd/system/
+  cp -rf * /home/darksocks/darksocks/
+  if [ ! -f /etc/systemd/system/darksocks.service ];then
+    cp -f darksocks.service /etc/systemd/system/
   fi
-  mkdir -p /etc/bsrouter
-  if [ ! -f /etc/bsrouter/bsrouter.json ];then
-    cp -f default-bsrouter.json /etc/bsrouter/bsrouter.json
+  mkdir -p /etc/darksocks
+  if [ ! -f /etc/darksocks/darksocks.json ];then
+    cp -f default-darksocks.json /etc/darksocks/darksocks.json
   fi
-  systemctl enable bsrouter.service
-}
-
-installClient(){
-  cd `dirname ${0}`
-  ln -sf bs-ssh.sh bs-ssh
-  ln -sf bs-scp.sh bs-scp
-  ln -sf bs-sftp.sh bs-sftp
-  ln -sf bsconsole bs-ping
-  ln -sf bsconsole bs-state
-  ln -sf bsconsole bs-bash
+  systemctl enable darksocks.service
 }
 
 case "$1" in
   -i)
-    case "$2" in
-    -s)
-      installServer
-      ;;
-    -c)
-      installClient
-      ;;
-    esac
+    installServer
     ;;
   *)
-    echo "Usage: ./bsrouter-install.sh -i"
+    echo "Usage: ./darksocks-install.sh -i"
     ;;
 esac
